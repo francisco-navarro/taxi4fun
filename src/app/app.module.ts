120e1431f7d9f6221b85e0179375915973081ea9
Paco Navarro <packarbell@gmail.com>
2017-06-16 15:48:23 +0200

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { WelcomeComponent } from './components/welcome.component';
 
 @NgModule({
+    imports: [
+      BrowserModule,
+      FormsModule,
+  //   RouterModule.forRoot([
+  //     {
+  //       path: 'detail/:id',
+  //       component: HeroDetailComponent
+  //     },
+  //     {
+  //       path: 'heroes',
+  //       component: HeroesComponent
+  //     },
+  //     {
+  //       path: 'dashboard',
+  //       component: DashboardComponent
+  //     },
+  //     {
+  //       path: '',
+  //       redirectTo: '/dashboard',
+  //       pathMatch: 'full'
+  //     }
+  //   ])
+  ],
   declarations: [
     WelcomeComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [WelcomeComponent]
