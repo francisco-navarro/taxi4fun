dd5de628641b54b9c10b86a349b398586f8a9a4f
Paco Navarro <packarbell@gmail.com>
2017-08-17 23:42:00 +0200

    Fix tests

--- src/app/views/register/register.component.spec.ts
+++ src/app/views/register/register.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,7 +10,8 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports: [ MaterialModule, BrowserAnimationsModule ]
     })
     .compileComponents();
   }));
