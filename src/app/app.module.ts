18310bfcf02bbcc9488c7baabdfda07b194c66f6
Paco Navarro <packarbell@gmail.com>
2017-09-28 20:30:53 +0200

    Migrating angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule }   from '@angular/router';
-// import { ChartsModule } from 'ng2-charts';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 
 import { ROUTES } from './app.routes';
 import { WelcomeComponent } from './views/welcome/welcome.component';
+import { MaterialModule } from 'app/core/material.module';
 
 @NgModule({
     imports: [
       BrowserModule,
-      BrowserAnimationsModule,
+      MaterialModule,
       FormsModule,
       HttpModule,
       ReactiveFormsModule,
-      // ChartsModule,
+      ChartsModule,
       RouterModule.forRoot(ROUTES),
   ],
   declarations: [
---
+++ src/app/core/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  exports: [
+    MatButtonModule
+  ]
+})
+export class MaterialModule {};
