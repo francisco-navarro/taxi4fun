f218dff6652f9be50b648462be0b54f4a3d602ea
Paco Navarro <pakonat@MacBook-Pro-de-Paco.local>
2017-10-28 21:32:17 +0200

    gesiter component

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,8 +1,15 @@
 import {NgModule} from '@angular/core';
 import { RegisterComponent } from './views/register/register.component';
+import { RouterModule }   from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import { ROUTES } from 'app/register/register.routes';
+
 
 @NgModule({
-  imports: [],
+  imports: [
+    MaterialModule,
+    RouterModule.forRoot(ROUTES),
+  ],
   exports: [],
   declarations: [RegisterComponent]
 })
