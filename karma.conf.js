c0b18e3536a4ad6e7c5c8901ed1b4cfed05bca97
Paco Navarro <pakonat@MacBook-Pro-de-Paco.local>
2017-10-29 12:51:46 +0100

    code smells

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,17 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
+var plugins = [
+  require('karma-jasmine'),
+  require('karma-chrome-launcher'),
+  require('karma-jasmine-html-reporter'),
+  require('karma-coverage-istanbul-reporter'),
+  require('@angular/cli/plugins/karma')
+];
 var config = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
+    plugins: plugins,
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
