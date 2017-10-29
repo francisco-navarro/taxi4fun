2381b3b4792e9b00cdc02d6e26cfc9ae1ef0cdf3
Paco Navarro <pakonat@MacBook-Pro-de-Paco.local>
2017-10-29 12:48:38 +0100

    imports in tests

--- src/app/register/views/register/register.component.spec.ts
+++ src/app/register/views/register/register.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MaterialModule } from '@angular/material';
 import { RegisterComponent } from './register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,7 +9,11 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports: [ 
+        MaterialModule,
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
   }));
