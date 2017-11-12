020f82eb60061bd95a6d943ae94ff9296cd54618
Paco Navarro <packarbell@gmail.com>
2017-11-12 12:35:02 +0100

    solucionar problema de dependencias y versiones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule }   from '@angular/router';
-// import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { ROUTES } from './app.routes';
