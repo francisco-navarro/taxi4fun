fa17764ad0bdccb8b8c337561cc42b35159d8e24
Navarro Lain <packarbell@gmail.com>
2017-11-15 15:26:36 +0100

    event emmiter

--- src/app/cars/components/car-list/car-list.component.ts
+++ src/app/cars/components/car-list/car-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'y4f-car-list',
@@ -7,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CarDashboardComponent implements OnInit {
 
   @Input() cars;
+  @Output() carClicked = new EventEmitter();
+  @Output() addNew = new EventEmitter();
 
   constructor() { }
 
