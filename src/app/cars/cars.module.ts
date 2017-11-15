f61ac474095410521d7dce3491b98ad73d67c45d
Navarro Lain <feno@Macbook15-Paco.local>
2017-11-15 15:22:20 +0100

    Car list

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarDashboardComponent } from './views/dashboard/car-dashboard.component';
 import { CarComponent } from './components/car/car.component';
+import { CarListComponent } from './components/car-list/car-list.component';
 import { ROUTES } from './cars.routes';
 import { RouterModule } from '@angular/router';
 
@@ -13,7 +14,8 @@ import { RouterModule } from '@angular/router';
     RouterModule.forRoot(ROUTES)
   ],
   entryComponents: [
-    CarComponent
+    CarComponent,
+    CarListComponent
   ],
   declarations: [
     CarDashboardComponent,
---
+++ src/app/cars/components/car-list/car-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'y4f-car-list',
+  templateUrl: './car-dashboard.component.html'
+})
+export class CarDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
