53604ce9f7c2061ec8ee9d8b63aaa8c0c29afcd1
Paco Navarro <packarbell@gmail.com>
2017-11-16 20:12:20 +0100

    fixed material dialog tests

--- src/app/cars/views/dashboard/car-dashboard.component.spec.ts
+++ src/app/cars/views/dashboard/car-dashboard.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarDashboardComponent } from './car-dashboard.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material';
+
+class MatDialogRefMock {
+
+}
 
 describe('CarDashboardComponent', () => {
   let component: CarDashboardComponent;
@@ -8,7 +14,11 @@ describe('CarDashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CarDashboardComponent ]
+      declarations: [ CarDashboardComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: MatDialog, useClass: MatDialogRefMock }
+      ]
     })
     .compileComponents();
   }));
